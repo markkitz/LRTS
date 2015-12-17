75fc79b254abf36c4ff1d8e88feb50f5ed866813
markkitz <me@markkitz.com>
2015-12-17 16:14:40 -0700

    added files

---
+++ index.jsx
@@ -0,0 +1,45 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const {Component } = React;
+
+
+const Header = () => {
+	return(
+		<div className="hdr">header</div>	
+	);
+}
+
+const Wrapper = ({test}) =>{
+	console.log(test);
+	return (
+
+		<div>
+			testeeeee
+		</div>
+	);
+}
+
+
+
+
+const Test1Comp = ({testProp}) => {
+	var test = 123;
+	console.log(testProp);
+	return(
+		<div className="test1" >
+			<Header/>
+			<div>{testProp}</div>
+			<Wrapper test={'test'} />
+		</div>
+	);
+}
+
+
+
+const render = () => {
+	ReactDOM.render(		
+		<Test1Comp testProp={'Mark Kitz'}  />, document.getElementById('root')
+	);	
+};
+
+render();
